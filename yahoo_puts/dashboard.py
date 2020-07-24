76bc4226f0f5f6764e6a3b45d60f4702251d2752
Sean Aye <hello@seanaye.ca>
2020-07-23 22:14:26 -0400

    add download button

--- yahoo_puts/dashboard.py
+++ yahoo_puts/dashboard.py
@@ -1,12 +1,24 @@
 from PIL import Image
 import streamlit as st
 import yfinance as yf
+import base64
 
 
 @st.cache
 def get_ticker(ticker: str, call_put: str, time: str):
     chain = ticker.option_chain(time)
     return chain.calls if call_put == 'calls' else chain.puts
+
+
+@st.cache
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    return f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
 
 
 table_type = ['calls', 'puts']
@@ -33,4 +45,11 @@ date = st.sidebar.selectbox(
     options_dates
 )
 
-st.dataframe(get_ticker(cur_ticker, call_put, date))
+cur_frame = get_ticker(cur_ticker, call_put, date)
+
+st.dataframe(cur_frame)
+
+st.markdown(get_table_download_link(cur_frame), unsafe_allow_html=True)
+
+
+
